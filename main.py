c9cb758d3f14dcc286571654cb5608e3d81bd90d
mlaporte <maxime.laporte@ynov.com>
2023-10-31 10:49:01 +0100

    boucle for vraiment employer

--- main.py
+++ main.py
@@ -26,9 +26,8 @@ class PlayerGameClient(Client):
                 self.add_command("0 EMPRUNTER 100000")
                 for _ in range(5):
                     self.add_command("0 ACHETER_CHAMP")
-                self.add_command("0 EMPLOYER")
-                self.add_command("0 EMPLOYER")
-                self.add_command("0 EMPLOYER")
+                for _ in range(25):
+                    self.add_command("0 EMPLOYER")
                 self.add_command("1 SEMER PATATE 3")
 
             self.send_commands()
