efc48169a792475742f5e272de07d25f637d1d3f
mlaporte <maxime.laporte@ynov.com>
2023-10-31 11:32:46 +0100

    test 3 arroser

--- main.py
+++ main.py
@@ -31,9 +31,8 @@ class PlayerGameClient(Client):
                 self.add_command("26 SEMER OIGNON 1")
                 self.add_command("27 SEMER PATATE 3")
                 self.add_command("28 SEMER COURGETTE 4")
-                for OUVRIER in range(1, 26):
-                    CHAMP = ((OUVRIER - 1) % 5) + 1
-                    self.add_command("{OUVRIER} ARROSER {CHAMP}")
+                for OUVRIER in range(1, 6):
+                    self.add_command("{OUVRIER} ARROSER 1")
                 
                 
 
