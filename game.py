c32a4ce3df7ce8a907048c36cda4706530007740
mlaporte <maxime.laporte@ynov.com>
2023-11-01 18:36:11 +0100

    mise à jour game.py

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ class Game:
             and stock["ZUCCHINI"] != 0
         ):
             for OUVRIER in range(31, 36):
-                    self.add_command(f"{OUVRIER} CUISINER")
+                self.add_command(f"{OUVRIER} CUISINER")
 
     def distribute_sawer(self):
         self.add_command("26 SEMER POIREAU 1")
@@ -29,4 +29,30 @@ class Game:
             vegetable_to_seed = vegetables[i % len(vegetables)]
             if field["content"] == "NONE":
                 self.add_command(f"{26 + i} SEMER {vegetable_to_seed} {i + 1}")
+    
+    def water(
+        self,
+        need_water_1,
+        need_water_2,
+        need_water_3,
+        need_water_4,
+        need_water_5,
+        farmer_id,
+        farmer_location,
+    ):
+        if need_water_1 != 0:
+            if farmer_id <= 5:
+                self.add_command(f"{farmer_id} ARROSER 1")
+        if need_water_2 != 0:
+            if farmer_id > 5 and farmer_id <= 10:
+                self.add_command(f"{farmer_id} ARROSER 2")
+        if need_water_3 != 0:
+            if farmer_id > 10 and farmer_id <= 15:
+                self.add_command(f"{farmer_id} ARROSER 3")
+        if need_water_4 != 0:
+            if farmer_id > 15 and farmer_id <= 20:
+                self.add_command(f"{farmer_id} ARROSER 4")
+        if need_water_5 != 0:
+            if farmer_id > 20 and farmer_id <= 25:
+                self.add_command(f"{farmer_id} ARROSER 5")
     
