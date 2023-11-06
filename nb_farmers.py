8045ff75bc5d0865f654bf72f62e67cdfec0b5b9
Madelpech <aimy.madelpech@ynov.com>
2023-11-06 14:31:26 +0100

    number farmers per field

---
+++ nb_farmers.py
@@ -0,0 +1,20 @@
+class Farmer:
+
+    def __init__(self, id, field):
+        self.id = id
+        self.field = field
+        self.free = True
+        
+    def farmers_in_field_1(self, id, field):
+        field.farmers = Farmer(2, 1)
+
+    def sow(self, field: int, vegetable):
+        self.free = False
+
+
+def nb_farmers_on_field(farmers, field):
+    compteur = 0
+    for farmer in farmers:
+        if farmer.field == field: 
+            compteur += 1 
+    return compteur
