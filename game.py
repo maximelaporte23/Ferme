7aa29f8c03aaf456327b6011dac0f7e6c6fe6246
mlaporte <maxime.laporte@ynov.com>
2023-11-06 14:32:51 +0100

    commencer par patate v2

--- game.py
+++ game.py
@@ -20,7 +20,7 @@ class Game:
     def distribute_sawer(self):
         self.add_command("26 SEMER PATATE 1")
         self.add_command("27 SEMER PATATE 2")
-        self.add_command("28 SEMER PATATE 2")
+        self.add_command("28 SEMER PATATE 3")
         self.add_command("29 SEMER PATATE 4")
         self.add_command("30 SEMER PATATE 5")
 
