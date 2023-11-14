e84777c2ec93fb0f8da7643066ddc0cc60d0ab58
mlaporte <maxime.laporte@ynov.com>
2023-11-14 10:56:28 +0100

    nombre employe

--- notre_ferme/main.py
+++ notre_ferme/main.py
@@ -37,7 +37,7 @@ class PlayerGameClient(Client):
                     self.game.add_command("0 ACHETER_CHAMP")
                 for _ in range(5):
                     self.game.add_command("0 ACHETER_TRACTEUR")
-                for _ in range(1, 37):
+                for _ in range(1, 39):
                     self.game.add_command("0 EMPLOYER")
                 self.game.distribute_sawer(fields=fields_json)
                 self.game.distribute_farmers()
