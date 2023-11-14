3950192ea328b38ac97bdf4064e933aa59ec1e5d
mlaporte <maxime.laporte@ynov.com>
2023-11-14 15:28:15 +0100

    launch

--- _launch.sh
+++ _launch.sh
@@ -1,4 +1,1 @@
-python3 -m notre_ferme.main -a localhost -p $1&
-python3 -m notre_ferme.tracteur -a localhost -p $1&
-python3 -m notre_ferme.tracteur2 -a localhost -p $1&
-python3 -m notre_ferme.tracteur3 -a localhost -p $1
+python3 -m notre_ferme.main -a localhost -p $1
