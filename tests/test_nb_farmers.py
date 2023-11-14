7f9c6ebccfb951ed46862af2c467d4d20721508c
mlaporte <maxime.laporte@ynov.com>
2023-11-14 11:04:02 +0100

    modif test

--- tests/test_nb_farmers.py
+++ tests/test_nb_farmers.py
@@ -4,35 +4,64 @@ from notre_ferme.nb_farmers import Farmer, nb_farmers_on_field
 def test_farmers_in_field_1():
     farmer = Farmer(2, 1)
     assert farmer.id == 2
-    assert farmer.field == 1    
-    # assert farmer.free == True
+    assert farmer.field == 1
 
 
 def test_nb_farmers_field_1():
-    farmers = [Farmer(1, 1), Farmer(2, 1), Farmer(3, 1), Farmer(4, 1), Farmer(5, 1), Farmer(6, 1)]
+    farmers = [
+        Farmer(1, 1),
+        Farmer(2, 1),
+        Farmer(3, 1),
+        Farmer(4, 1),
+        Farmer(5, 1),
+        Farmer(6, 1)
+    ]
     assert nb_farmers_on_field(farmers, 1) == 6 or 7
 
 
 def test_nb_farmers_field_2():
-    farmers = [Farmer(1, 2), Farmer(2, 2), Farmer(3, 2), Farmer(4, 2), Farmer(5, 2), Farmer(6, 2)]
+    farmers = [
+        Farmer(1, 2),
+        Farmer(2, 2),
+        Farmer(3, 2),
+        Farmer(4, 2),
+        Farmer(5, 2),
+        Farmer(6, 2)
+    ]
     assert nb_farmers_on_field(farmers, 2) == 6 or 7
 
 
 def test_nb_farmers_field_3():
-    farmers = [Farmer(1, 3), Farmer(2, 3), Farmer(3, 3), Farmer(4, 3), Farmer(5, 3), Farmer(6, 3)]
+    farmers = [
+        Farmer(1, 3),
+        Farmer(2, 3),
+        Farmer(3, 3),
+        Farmer(4, 3),
+        Farmer(5, 3),
+        Farmer(6, 3)
+    ]
     assert nb_farmers_on_field(farmers, 3) == 6 or 7
 
 
 def test_nb_farmers_field_4():
-    farmers = [Farmer(1, 4), Farmer(2, 4), Farmer(3, 4), Farmer(4, 4), Farmer(5, 4), Farmer(6, 4)]
+    farmers = [
+        Farmer(1, 4),
+        Farmer(2, 4),
+        Farmer(3, 4),
+        Farmer(4, 4),
+        Farmer(5, 4),
+        Farmer(6, 4)
+    ]
     assert nb_farmers_on_field(farmers, 4) == 6 or 7
 
 
 def test_nb_farmers_field_5():
-    farmers = [Farmer(1, 5), Farmer(2, 5), Farmer(3, 5), Farmer(4, 5), Farmer(5, 5), Farmer(6, 5)]
+    farmers = [
+        Farmer(1, 5),
+        Farmer(2, 5),
+        Farmer(3, 5),
+        Farmer(4, 5),
+        Farmer(5, 5),
+        Farmer(6, 5)
+    ]
     assert nb_farmers_on_field(farmers, 5) == 6 or 7
-
-
-
-    
-
