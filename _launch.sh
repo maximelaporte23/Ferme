4858e440eb55cc92bdbbb38d19b01ae1a2344051
GCelia <celia.gonzalez@ynov.com>
2023-11-14 12:20:53 +0100

    4x notre code

--- _launch.sh
+++ _launch.sh
@@ -1,4 +1,4 @@
-python3 -m notre_ferme.main -a localhost -p $1
-python3 -m notre_ferme.main -a localhost -p $2
-python3 -m notre_ferme.main -a localhost -p $3
-python3 -m notre_ferme.main -a localhost -p $4
+python3 -m notre_ferme.main -a localhost -p $1&
+python3 -m notre_ferme.main -a localhost -p $1&
+python3 -m notre_ferme.main -a localhost -p $1&
+python3 -m notre_ferme.main -a localhost -p $1&
